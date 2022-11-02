778a4493b3b21be541170e81b08c8712486f10bd
Brehiner Andres Quiros Naranjo <89605932+brehinerqn@users.noreply.github.com>
2022-11-02 16:00:10 -0500

    2

---
+++ src/components/admin/EquipoFoto.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <video ref="video" @canplay="initCanvas()">stream unavailable</video>
+        <button @click="takePicture()">take</button>
+        <canvas ref="canvas" style="display:none;" />
+
+        <!--  -->
+
+    </div>
+
+</template>
+<script>
+export default {
+
+    mounted() {
+        this.canvas = this.$refs.canvas
+        this.video = this.$refs.video
+        this.startCapture()
+
+
+    },
+    methods: {
+        startCapture() {
+            
+                navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(stream => {
+                    this.video.srcObject = stream
+                    this.video.play()
+                }).catch(error => {
+                    console.log(error)
+                })
+            
+            
+
+        },
+        takePicture() {
+
+            let context = this.canvas.getContext('2d')
+            context.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight)
+            this.$emit('picture-taken', this.canvas.toDataURL('image/png'))
+            
+            this.video.srcObject.getTracks().forEach((track) => {
+                track.stop();
+            });
+            this.video.srcObject = null;
+
+            console.log(this.canvas.toDataURL('image/png'))
+
+        },
+
+        initCanvas() {
+            this.canvas.setAttribute('width', this.video.videoWidth)
+            this.canvas.setAttribute('height', this.video.videoHeight)
+        }
+    },
+    data() {
+        return {
+            video: null,
+            canvas: null,
+            
+
+
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/admin/Equipos.vue
+++ src/components/admin/Equipos.vue
@@ -1,56 +1,40 @@
 <template>
-    <div>
-        <video ref="video">stream unavailable</video>
-        <button @click="takePicture">take</button>
-        <canvas ref="canvas" style="display:none;" />
-        <img :src="imageSrc" />
-        <!-- <Equipos @picture-taken="imageSrc= $event"/> -->
 
+    <div class="container-body">
+        <EquipoFoto @picture-taken="imageSrc= $event"/>
+        <img :src="imageSrc" class="vick" />
+
+      
     </div>
-    
+
 </template>
+
+
+
+<style>
+
+
+</style>
+
+
 <script>
+import EquipoFoto from "./EquipoFoto.vue"
 export default {
-    name: "Equipos",
-    mounted() {
-        this.canvas = this.$refs.canvas
-        this.video = this.$refs.video
-        this.startCapture()
 
-    },
-    methods: {
-        startCapture() {
-            navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(stream => {
-                this.video.srcObject = stream
-                this.video.play()
-            }).catch(error => {
-                console.log(error)
-            })
-
-        },
-        takePicture() {
-            let context = this.canvas.getContext('2d')
-            context.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight)
-            this.$emit('picture-taken', this.canvas.toDataURL('image/png'))
-
-        },
-
-        initCanvas() {
-            this.canvas.setAttribute('width', this.videoWidth)
-            this.canvas.setAttribute('height', this.videoHeight)
-        }
+    components:{
+        EquipoFoto
     },
     data() {
         return {
-            video: null,
-            canvas: null,
-            imageSrc:null
-
-        }
+            imageSrc:null,
+        };
     },
-}
+    mounted() {
+    },
+
+    methods: {
+    },
+};
+
+
 </script>
-
-<style scoped>
-
-</style>
