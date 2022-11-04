c15575a5962d93b197594f1bd9c0cadc0aebbde1
Manuel Stiven Pérez Victoria <89532383+Vick31@users.noreply.github.com>
2022-11-04 16:48:24 -0500

    ac

--- src/components/admin/RegisterUser.vue
+++ src/components/admin/RegisterUser.vue
@@ -1,65 +1,99 @@
 <template>
-    <div class="div-form">
-        <form>
-            <h1 class="title">Registrar usuario</h1>
-            
-            <div class="form-floating pb-3">
-                <input type="number" class="form-control" name="name" v-model="form.dni" />
-                <label for="floatingInput">Dni</label>
-                <span v-if="errors.dni">{{ errors.dni[0] }}</span>
-            </div>
-            <div class="form-floating pb-3">
-                <input type="name" class="form-control" name="name" v-model="form.name" />
-                <label for="floatingInput">Nombre</label>
-                <span v-if="errors.name">{{ errors.name[0] }}</span>
-            </div>
+  <div class="div-form">
+    <form>
+      <h1 class="title">Registrar usuario</h1>
 
-            <div class="form-floating pb-3">
-                <input type="number" class="form-control" name="name" v-model="form.phone_number" />
-                <label for="floatingInput">Numero de telefono</label>
-                <span v-if="errors.phone_number">{{ errors.phone_number[0] }}</span>
-            </div>
+      <div class="form-floating pb-3">
+        <input
+          type="number"
+          class="form-control"
+          name="name"
+          v-model="form.dni"
+        />
+        <label for="floatingInput">Dni</label>
+        <span v-if="errors.dni">{{ errors.dni[0] }}</span>
+      </div>
+      <div class="form-floating pb-3">
+        <input
+          type="name"
+          class="form-control"
+          name="name"
+          v-model="form.name"
+        />
+        <label for="floatingInput">Nombre</label>
+        <span v-if="errors.name">{{ errors.name[0] }}</span>
+      </div>
 
-            <div class="form-floating pb-3">
-                <input type="text" class="form-control" name="email" v-model="form.email" />
-                <label for="floatingInput">Correo electronico</label>
-                <span v-if="errors.email">{{ errors.email[0] }}</span>
-            </div>
-            <!-- <div class="form-floating pb-3">
+      <div class="form-floating pb-3">
+        <input
+          type="number"
+          class="form-control"
+          name="name"
+          v-model="form.phone_number"
+        />
+        <label for="floatingInput">Numero de telefono</label>
+        <span v-if="errors.phone_number">{{ errors.phone_number[0] }}</span>
+      </div>
+
+      <div class="form-floating pb-3">
+        <input
+          type="text"
+          class="form-control"
+          name="email"
+          v-model="form.email"
+        />
+        <label for="floatingInput">Correo electronico</label>
+        <span v-if="errors.email">{{ errors.email[0] }}</span>
+      </div>
+      <!-- <div class="form-floating pb-3">
                 <input type="text" class="form-control" name="email" v-model="form.companies_id" />
                 <label for="floatingInput">compania</label>
                 <span v-if="errors.companies_id">{{ errors.companies_id[0] }}</span>
             </div> -->
 
-            <div class="div-row">
-                <div class="form-floating pb-3">
-                    <input type="password" class="form-control" name="" v-model="form.password" />
-                    <label for="floatingInput">Contraseña</label>
-                    <span v-if="errors.password">{{ errors.password[0] }} </span>
-                </div>
-                <div class="form-floating pb-3">
-                    <input type="password" class="form-control" name="" v-model="form.password_confirmation" />
-                    <label for="floatingInput">Confirmar contraseña </label>
-                    <span v-if="errors.password_confirmation">{{ errors.password_confirmation[0] }} </span>
-                </div>
-            </div>
-            <div class="form-footer">
-                <router-link class="rotes" to="/usuarios">
-                    <button type="button" class="btn btn-primary save">
-                        Regresar
-                    </button>
-                </router-link>
+      <div class="div-row">
+        <div class="form-floating pb-3">
+          <input
+            type="password"
+            class="form-control"
+            name=""
+            v-model="form.password"
+          />
+          <label for="floatingInput">Contraseña</label>
+          <span v-if="errors.password">{{ errors.password[0] }} </span>
+        </div>
+        <div class="form-floating pb-3">
+          <input
+            type="password"
+            class="form-control"
+            name=""
+            v-model="form.password_confirmation"
+          />
+          <label for="floatingInput">Confirmar contraseña </label>
+          <span v-if="errors.password_confirmation"
+            >{{ errors.password_confirmation[0] }}
+          </span>
+        </div>
+      </div>
+      <div class="form-footer">
+        <router-link class="rotes" to="/usuarios">
+          <button type="button" class="btn btn-primary save">Regresar</button>
+        </router-link>
 
-                <router-link class="rotes" to="/usuarios" >
-                    <button type="button" id="rot" @click="register_admin()" class="btn btn-primary save">
-                        Guardar
-                    </button>
-                </router-link>
-            </div>
-            <p v-if="message">{{ message }}</p>
-        </form>
-    </div>
-
+        <router-link class="rotes" to="/usuarios">
+          <button
+            type="button"
+            id="rot"
+            @click="register_admin()"
+            class="btn btn-primary save"
+          >
+            Guardar
+          </button>
+        </router-link>
+      </div>
+      <p v-if="message">{{ message }}</p>
+    </form>
+  </div>
 </template>
   
   
@@ -68,7 +102,6 @@
 </style>
   
 <script>
-
 export default {
     data() {
         return {
@@ -81,7 +114,7 @@ export default {
                 email: "",
                 password: "",
                 password_confirmation: "",
-                companies_id: "1",
+                companies_id: "2",
                 roles_id: "2",
             },
             errors: {},
@@ -91,11 +124,11 @@ export default {
     },
     mounted() {
 
-        if (localStorage.token) {
-            
-            this.token = localStorage.token;
-            this.index()
+        this.get_token()
 
+        if (localStorage.token) {
+            this.token = localStorage.token;
+            this.get_user();
         } else {
             this.$router.push({
                 name: "Login",
@@ -104,9 +137,37 @@ export default {
                 }
             })
         }
+        this.index()
     },
 
     methods: {
+
+        async get_token() {
+            await axios.get("http://127.0.0.1:8000/sanctum/csrf-cookie")
+        },
+
+        async index() {
+            let response = await axios.get("http://127.0.0.1:8000/api/roles")
+            this.roles_list = response.data
+        },
+        async get_user() {
+
+            try {
+                const rs = await this.axios.get('/api/user', {
+                    headers: { Authorization: `Bearer ${this.token}` },
+                });
+                this.user = rs.data.user;
+            }
+
+            catch (e) {
+                this.$router.push({
+                    name: "Login",
+                    params: {
+                        message: "No estas autorizado para acceder con esta cuenta"
+                    }
+                })
+            }
+        },
 
         async register_admin() {
             try {
